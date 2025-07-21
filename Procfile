aa51256077ebf089c8ed816e5b3143f0d22a6158
Simon Velasquez c <84815562+matryx-root@users.noreply.github.com>
2025-07-21 19:58:44 -0400

    Actualización para producción

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
 web: node server.js
+release: npm run prisma:migrate
--- server.js
+++ server.js
@@ -1,12 +1,6 @@
-// server.js
 require('dotenv').config();
-const app = require('./app'); // Importa la configuración completa del servidor (middlewares, rutas, frontend, errores)
-
+const app = require('./app');
 const PORT = process.env.PORT || 3000;
-const path = require('path');
-
-console.log('Path absoluto a frontend/build/index.html:', path.join(__dirname, 'frontend', 'build', 'index.html'));
-
 
 app.listen(PORT, () => {
   console.log(`✅ Servidor corriendo en puerto ${PORT}`);
