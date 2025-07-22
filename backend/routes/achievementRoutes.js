5adcb990c039a4879d7abfc3e3fb197ecae3ae0d
Simon Velasquez c <84815562+matryx-root@users.noreply.github.com>
2025-07-22 18:39:08 -0400

    Fix: ruta achievement corregida

--- backend/routes/achievementRoutes.js
+++ backend/routes/achievementRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const achievementController = require('../controllers/achievementController');
 const { authMiddleware } = require('../middlewares/auth');
 
-router.post('/', authMiddleware, achievementController.crearAchievement);
-router.get('/usuario/:usuarioId', achievementController.listarAchievementsUsuario);
 
+router.get('/usuario/:usuarioId', achievementController.listarAchievementsUsuario);
+router.post('/', authMiddleware, achievementController.crearAchievement);
 module.exports = router;
