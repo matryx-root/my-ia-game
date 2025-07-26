774bb07667007bf4209726f5b7254c7fe8202b7a
Simon Velasquez c <84815562+matryx-root@users.noreply.github.com>
2025-07-26 15:47:09 -0400

    Aplicacion de estilos y responsive

--- frontend/src/components/LandingPage.js
+++ frontend/src/components/LandingPage.js
@@ -24,7 +24,7 @@ export default function LandingPage() {
           className="landing-btn"
           onClick={() => navigate("/login")}
         >
-          ¡Quiero jugar Rosita!
+          ¡Quiero jugar Perlita!
         </button>
 
         <div className="landing-note">
