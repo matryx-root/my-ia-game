d4a1b1806f1328c453a6fe8fe3e8153251d1e568
Simon Velasquez c <84815562+matryx-root@users.noreply.github.com>
2025-07-26 12:07:24 -0400

    Aplicacion de estilos y responsive

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route, Navigate, useLocation } from "react-router-dom";
-import './App.css'; 
 import LandingPage from "./components/LandingPage";
 import Login from "./components/Login";
 import Registro from "./components/Registro";
